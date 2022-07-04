14c64f2fea7db1d2a8413513441e7c71dc861250
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-07-04 21:02:15 +0300

    Time: 514 ms (12.50%), Space: 46.2 MB (12.50%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.kt
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.kt
@@ -1,14 +1,14 @@
 class Solution {
     fun sortEvenOdd(nums: IntArray): IntArray {
-        val oddIndicesList = mutableListOf<Int>()
-        val evenIndicesList = mutableListOf<Int>()
+        val oddIndicesList = nums.filterIndexed { index, item -> index % 2 != 0}.sortedDescending()
+        val evenIndicesList = nums.filterIndexed { index, item -> index % 2 == 0}.sorted()
         
-        nums.forEachIndexed { index, item -> 
-            if (index % 2 == 0) evenIndicesList.add(item) 
-            else oddIndicesList.add(item)
-        }
-        oddIndicesList.sortDescending()
-        evenIndicesList.sort()
+        // nums.forEachIndexed { index, item -> 
+        //     if (index % 2 == 0) evenIndicesList.add(item) 
+        //     else oddIndicesList.add(item)
+        // }
+        // oddIndicesList.sortDescending()
+        // evenIndicesList.sort()
         
         val oddIterator = oddIndicesList.iterator()
         val evenIterator = evenIndicesList.iterator()
