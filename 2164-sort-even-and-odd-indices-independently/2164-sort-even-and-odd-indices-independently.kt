6acb60a21787fc89eed27fd9253d5b02c5687991
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-07-04 21:09:55 +0300

    Time: 492 ms (12.50%), Space: 43.4 MB (12.50%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.kt
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.kt
@@ -1,26 +1,26 @@
 class Solution {
     fun sortEvenOdd(nums: IntArray): IntArray {
-        val oddIndicesList = nums.filterIndexed { index, item -> index % 2 != 0}.sortedDescending()
-        val evenIndicesList = nums.filterIndexed { index, item -> index % 2 == 0}.sorted()
+        val oddIndicesList = nums.filterIndexed { index, item -> 
+            index % 2 != 0}.sortedDescending()
+        val evenIndicesList = nums.filterIndexed { index, item -> 
+            index % 2 == 0}.sorted()
         
-        // nums.forEachIndexed { index, item -> 
-        //     if (index % 2 == 0) evenIndicesList.add(item) 
-        //     else oddIndicesList.add(item)
-        // }
-        // oddIndicesList.sortDescending()
-        // evenIndicesList.sort()
+        // val oddIterator = oddIndicesList.iterator()
+        // val evenIterator = evenIndicesList.iterator()
         
-        val oddIterator = oddIndicesList.iterator()
-        val evenIterator = evenIndicesList.iterator()
+        val result = IntArray(nums.size)
         
-        val result = mutableListOf<Int>()
-        while (oddIterator.hasNext() || evenIterator.hasNext()) {
-            if(evenIterator.hasNext())
-            result.add(evenIterator.next())
-            if(oddIterator.hasNext())
-            result.add(oddIterator.next())
+        for (i in nums.indices) {
+            result[i] = if (i % 2 == 0) evenIndicesList[i / 2] else oddIndicesList[i / 2]
         }
         
-        return result.toIntArray()
+        // while (oddIterator.hasNext() || evenIterator.hasNext()) {
+        //     if(evenIterator.hasNext())
+        //     result.add(evenIterator.next())
+        //     if(oddIterator.hasNext())
+        //     result.add(oddIterator.next())
+        // }
+        
+        return result
     }
 }
