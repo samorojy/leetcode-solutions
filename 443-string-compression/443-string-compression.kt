72ba96d89d4bc5c342d88b9f6871533515955521
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-07-20 20:32:52 +0300

    Time: 309 ms (60.71%), Space: 37.6 MB (85.71%) - LeetHub

---
+++ 443-string-compression/443-string-compression.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun compress(chars: CharArray): Int {
+      var indexAns = 0
+        var index = 0
+        while (index < chars.size) {
+            val currentChar = chars[index]
+            var count = 0
+            while (index < chars.size && chars[index] == currentChar) {
+                index++
+                count++
+            }
+            chars[indexAns++] = currentChar
+            if (count != 1) for (c in count.toString().toCharArray()) chars[indexAns++] = c
+        }
+        return indexAns  
+    }
+}
