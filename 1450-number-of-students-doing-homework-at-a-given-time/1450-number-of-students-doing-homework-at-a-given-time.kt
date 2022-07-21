3d8ba3463fc558e8769f3e7258d85955b62e24e9
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-07-21 22:29:32 +0300

    Time: 250 ms (72.41%), Space: 34.7 MB (89.66%) - LeetHub

--- 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.kt
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.kt
@@ -2,7 +2,7 @@ class Solution {
     fun busyStudent(startTime: IntArray, endTime: IntArray, queryTime: Int): Int {
         var count = 0
         startTime.forEachIndexed { index, item ->
-            if (queryTime in startTime[index]..endTime[index]) {
+            if (queryTime >= startTime[index] && queryTime <= endTime[index]) {
                 count++
             }
         }
