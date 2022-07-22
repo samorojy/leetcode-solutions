9f41c900b7039b7e56315a1b3dd75159891c9c33
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-07-22 12:42:24 +0300

    Time: 2831 ms (5.07%), Space: 88.8 MB (7.18%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun checkInclusion(s1: String, s2: String): Boolean {
+        val s1Map = mutableMapOf<Char, Int>()
+        for (c in s1) {
+            s1Map[c] = s1Map.getOrDefault(c, 0) + 1
+        }
+        
+        for (i in 0..(s2.length - s1.length)) {
+            val s2Map = mutableMapOf<Char, Int>()
+            for (c in i..(s1.length + i - 1)) {
+                s2Map[s2[c]] = s2Map.getOrDefault(s2[c], 0) + 1
+            }
+            println("->>${s2Map.size}")
+            print(i)
+            if (isSimilar(s1Map, s2Map)) return true
+        }
+        return false
+    }
+    
+    fun isSimilar(firstMap: Map<Char, Int>, secondMap: Map<Char, Int>): Boolean {
+        for ((key, value) in firstMap) {
+                if (secondMap[key] != value) {
+                    return false
+                }
+        }
+        return true
+    }
+}
