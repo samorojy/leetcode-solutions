2def8aa90d1132ff63c9db88c20b0c8bdd5fe9a5
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-07-22 18:17:16 +0300

    Time: 592 ms (9.11%), Space: 47.1 MB (10.31%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.kt
+++ 125-valid-palindrome/125-valid-palindrome.kt
@@ -2,6 +2,7 @@ class Solution {
     fun isPalindrome(s: String): Boolean {
         if (s.trim().isEmpty())
             return true
+        //Can use 2 pointers two O(n) solution
         val alphanumeric = s.replace("[^A-Za-z0-9]".toRegex(), "").toLowerCase()
         if (alphanumeric.isEmpty())
             return true
