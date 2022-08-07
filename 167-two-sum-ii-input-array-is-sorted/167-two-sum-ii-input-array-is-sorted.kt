14d860c7eda2036df5685f2b7f05a98f079ef25d
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-08-07 21:55:57 +0300

    Time: 910 ms (12.15%), Space: 70.3 MB (5.36%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var l = 0
+        var r = numbers.lastIndex
+        val ans = IntArray(2)
+        while (numbers[l] + numbers[r] != target && l < r) {
+            println(r)
+            println(l)
+            if (numbers[l] + numbers[r] > target) {
+                r--
+            } else {
+                l++
+            }
+        }
+        ans[0] = l + 1
+        ans[1] = r + 1
+        return ans
+    }
+}
